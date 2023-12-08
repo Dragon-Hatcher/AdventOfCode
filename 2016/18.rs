86abbbf566a81f0036d2e401ccec58ba5c77c752
Daniel James <danieldragonhatcher@gmail.com>
2023-12-07 19:39:13 -0800

    2016 / 18 bitwise

--- 2016/18.rs
+++ 2016/18.rs
@@ -1,5 +1,3 @@
-use std::iter::once;
-
 use advent::prelude::*;
 
 fn default_input() -> &'static str {
@@ -7,27 +5,30 @@ fn default_input() -> &'static str {
 }
 
 fn solve(input: &str, rows: usize) -> i64 {
-    let mut row = input.trim().chars().map(|c| c == '^').collect_vec();
-    let mut safe_cnt = row.iter().filter(|t| !**t).count();
+    let len = input.chars().count();
+    let mask = ((1 << len) - 1) << 1; // 0111...1110
 
-    for _ in 1..rows {
-        row = chain!(once(false), row, once(false))
-            .tuple_windows()
-            .map(|(a, _, b)| a != b)
-            .collect();
+    let mut row = 0u128;
+    let mut safe_cnt = 0;
 
-        safe_cnt += row.iter().filter(|t| !**t).count();
+    for (i, c) in input.chars().enumerate() {
+        row |= ((c == '^') as u128) << (i + 1);
+    }
+
+    for _ in 0..rows {
+        safe_cnt += len - row.count_ones() as usize;
+        row = ((row << 1) ^ (row >> 1)) & mask;
     }
 
     safe_cnt as i64
 }
 
 fn part1(input: &str) -> i64 {
-    solve(input, 40)
+    solve(input.trim(), 40)
 }
 
 fn part2(input: &str) -> i64 {
-    solve(input, 400000)
+    solve(input.trim(), 400000)
 }
 
 fn main() {
