5304da67fd0c1d97abceb1b5a06c20364defc34a
Daniel James <danieldragonhatcher@gmail.com>
2023-12-21 13:33:15 -0800

    2017 / 07 clean

--- 2017/07.rs
+++ 2017/07.rs
@@ -4,7 +4,15 @@ fn default_input() -> &'static str {
     include_input!(2017 / 07)
 }
 
-fn parse(prog: &str) -> (&str, i64, Vec<&str>) {
+#[derive(Debug, Clone, Default)]
+struct Node<'a> {
+    weight: i64,
+    total_weight: i64,
+    children: Vec<&'a str>,
+    parent: Option<&'a str>,
+}
+
+fn parse_line(prog: &str) -> (&str, i64, Vec<&str>) {
     let (name, rest) = prog.split_once(" (").unwrap();
     let (weight, children) = rest.split_once(")").unwrap();
     let weight = weight.parse().unwrap();
@@ -16,134 +24,94 @@ fn parse(prog: &str) -> (&str, i64, Vec<&str>) {
     (name, weight, children)
 }
 
-fn part1(input: &str) -> &str {
-    let mut parents = HashMap::new();
+fn parse(input: &str) -> HashMap<&str, Node<'_>> {
+    let mut nodes: HashMap<&str, Node<'_>> = HashMap::new();
 
-    for (name, _, children) in input.lines().map(parse) {
-        for child in children {
-            parents.insert(child, name);
+    for (name, weight, children) in input.lines().map(parse_line) {
+        for &child in &children {
+            nodes.entry(child).or_default().parent = Some(name);
         }
+
+        let n = nodes.entry(name).or_default();
+        n.weight = weight;
+        n.children = children;
     }
 
-    let mut prog = *parents.keys().next().unwrap();
-    while let Some(parent) = parents.get(prog) {
-        prog = &parent;
+    nodes
+}
+
+fn root<'a>(nodes: &HashMap<&'a str, Node<'a>>) -> &'a str {
+    let mut root = *nodes.keys().nu();
+    while let Some(parent) = nodes.get(root).unwrap().parent {
+        root = parent;
     }
-    prog
+    root
+}
+
+fn part1(input: &str) -> &str {
+    let nodes = parse(input);
+    root(&nodes)
 }
 
 fn part2(input: &str) -> i64 {
-    let mut parents = HashMap::new();
-    let mut children = HashMap::new();
-    let mut weights = HashMap::new();
+    let mut nodes = parse(input);
 
-    for (name, weight, c) in input.lines().map(parse) {
-        for child in c.iter() {
-            parents.insert(*child, name);
+    fn find_weights(root: &str, nodes: &mut HashMap<&str, Node<'_>>) -> i64 {
+        let node = nodes.get_mut(root).unwrap();
+        if node.total_weight == 0 {
+            let child_weights: i64 = node
+                .children
+                .clone()
+                .iter()
+                .map(|c| find_weights(c, nodes))
+                .sum();
+            let node = nodes.get_mut(root).unwrap();
+            node.total_weight = node.weight + child_weights;
         }
-        children.insert(name, c);
-        weights.insert(name, weight);
+        let node = nodes.get_mut(root).unwrap();
+        node.total_weight
     }
 
-    let mut root = *parents.keys().next().unwrap();
-    while let Some(parent) = parents.get(root) {
-        root = &parent;
-    }
+    let root = root(&nodes);
+    find_weights(root, &mut nodes);
 
-    fn solve_weights<'a>(
-        root: &'a str,
-        children: &HashMap<&'a str, Vec<&'a str>>,
-        weights: &HashMap<&str, i64>,
-        tot_weights: &mut HashMap<&'a str, i64>,
-    ) -> i64 {
-        match tot_weights.get(root) {
-            Some(&w) => w,
-            None => {
-                let w = *weights.get(root).unwrap()
-                    + children
-                        .get(root)
-                        .unwrap()
-                        .iter()
-                        .map(|c| solve_weights(c, children, weights, tot_weights))
-                        .sum::<i64>();
-                tot_weights.insert(root, w);
-                w
-            }
-        }
-    }
-
-    fn solve(
-        node: &str,
-        target_weight: i64,
-        parents: &HashMap<&str, &str>,
-        children: &HashMap<&str, Vec<&str>>,
-        weights: &HashMap<&str, i64>,
-        tot_weights: &HashMap<&str, i64>,
-    ) -> Option<i64> {
-        let my_weight = *tot_weights.get(node).unwrap();
-        let my_children = children.get(node).unwrap();
-        let my_children_weights = my_children
+    fn solve(node: &str, target_weight: i64, nodes: &HashMap<&str, Node<'_>>) -> Option<i64> {
+        let me = nodes.get(node).unwrap();
+        let child_weights = me
+            .children
             .iter()
-            .map(|c| tot_weights.get(c).unwrap())
+            .map(|c| nodes.get(c).unwrap().total_weight)
             .collect_vec();
 
-        if my_children_weights.iter().all_equal() {
-            // children are balanced
-            if my_weight == target_weight {
-                // search my children
-                for c in my_children {
-                    let s = solve(
-                        c,
-                        *my_children_weights[0],
-                        parents,
-                        children,
-                        weights,
-                        tot_weights,
-                    );
-                    if s.is_some() {
-                        return s;
+        if child_weights.iter().all_equal() {
+            if me.total_weight == target_weight {
+                for (i, child) in me.children.iter().enumerate() {
+                    if let Some(w) = solve(*child, child_weights[i], nodes) {
+                        return Some(w);
                     }
                 }
-
                 None
             } else {
-                // this node is the incorrect one
-                Some(target_weight - my_children_weights.iter().copied().sum::<i64>())
+                Some(target_weight - child_weights.iter().sum::<i64>())
             }
         } else {
-            // children are unbalanced
             let mut counts: HashMap<i64, i64> = HashMap::new();
-            for &w in &my_children_weights {
-                *counts.entry(*w).or_default() += 1;
+            for &w in &child_weights {
+                *counts.entry(w).or_default() += 1;
             }
             let (most_common, _) = counts.into_iter().max_by_key(|(_, c)| *c).unwrap();
+            let (&unbalanced_child, _) = me
+                .children
+                .iter()
+                .zip(child_weights.iter())
+                .find(|(_, &w)| w != most_common)
+                .unwrap();
 
-            for (c, w) in my_children.iter().zip(my_children_weights.iter()) {
-                if **w != most_common {
-                    let s = solve(*c, most_common, parents, children, weights, tot_weights);
-                    if s.is_some() {
-                        return s;
-                    }
-                }
-            }
-
-            unreachable!()
+            solve(unbalanced_child, most_common, nodes)
         }
     }
 
-    let mut tot_weights = HashMap::new();
-    solve_weights(root, &children, &weights, &mut tot_weights);
-
-    let root_weight = *tot_weights.get(root).unwrap();
-    solve(
-        root,
-        root_weight,
-        &parents,
-        &children,
-        &weights,
-        &tot_weights,
-    )
-    .unwrap()
+    solve(root, nodes.get(root).unwrap().total_weight, &nodes).unwrap()
 }
 
 fn main() {
