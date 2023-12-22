56a9bd712d5d06213a08e29e3b3591aa1dc3c0a5
Daniel James <danieldragonhatcher@gmail.com>
2023-12-21 22:15:18 -0800

    2023 / 22 cleaner

--- 2023/22.rs
+++ 2023/22.rs
@@ -4,182 +4,107 @@ fn default_input() -> &'static str {
     include_input!(2023 / 22)
 }
 
-fn ps(b: (Vector3, Vector3)) -> HashSet<Vector3> {
-    let mut seen = HashSet::new();
+#[derive(Debug, Clone, Copy)]
+struct Brick(Vector3, Vector3);
 
-    for x in b.0.x..=b.1.x {
-        for y in b.0.y..=b.1.y {
-            for z in b.0.z..=b.1.z {
-                seen.insert(Vector3::new(x, y, z));
+impl Brick {
+    fn lower(mut self) -> Self {
+        self.0.z -= 1;
+        self.1.z -= 1;
+        self
+    }
+
+    fn above_ground(self) -> bool {
+        self.0.z > 0 && self.1.z > 0
+    }
+
+    fn points(self) -> impl Iterator<Item = Vector3> {
+        iproduct!(
+            self.0.x..=self.1.x,
+            self.0.y..=self.1.y,
+            self.0.z..=self.1.z
+        )
+        .map(|(x, y, z)| Vector3::new(x, y, z))
+    }
+}
+
+fn parse(input: &str) -> Vec<Brick> {
+    input
+        .lines()
+        .map(|l| {
+            let (x1, y1, z1, x2, y2, z2) = l.nums().tup();
+            Brick(Vector3::new(x1, y1, z1), Vector3::new(x2, y2, z2))
+        })
+        .collect()
+}
+
+fn create_map(bricks: &[Brick]) -> HashMap<Vector3, usize> {
+    let mut map: HashMap<Vector3, usize> = HashMap::new();
+
+    for (group, mut brick) in bricks.iter().copied().enumerate() {
+        while {
+            let low = brick.lower();
+            low.above_ground() && low.points().all(|p| !map.contains_key(&p))
+        } {
+            brick = brick.lower();
+        }
+
+        for p in brick.points() {
+            map.insert(p, group);
+        }
+    }
+
+    map
+}
+
+fn get_supports(map: &HashMap<Vector3, usize>) -> (HashMap<usize, HashSet<usize>>, HashMap<usize, HashSet<usize>>) {
+    let mut supporting: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut supported_by: HashMap<usize, HashSet<usize>> = HashMap::new();
+
+    for (p, &group) in map {
+        let down = Vector3::new(p.x, p.y, p.z - 1);
+        if let Some(&dg) = map.get(&down) {
+            if dg != group {
+                supporting.entry(dg).or_default().insert(group);
+                supported_by.entry(group).or_default().insert(dg);
             }
         }
     }
 
-    seen
+    (supporting, supported_by)
 }
 
 fn part1(input: &str) -> i64 {
-    let mut bricks = input
-        .lines()
-        .map(|l| {
-            let (x1, y1, z1, x2, y2, z2) = l.nums().tup();
-            (Vector3::new(x1, y1, z1), Vector3::new(x2, y2, z2))
-        })
-        .collect_vec();
+    let mut bricks = parse(input);
+    bricks.sort_by_key(|b| b.0.z);
 
-    bricks.sort_by_key(|x| x.0.z);
+    let map = create_map(&bricks);
+    let (supporting, supported_by) = get_supports(&map);
 
-    let mut top: HashMap<Vector3, usize> = HashMap::new();
-
-    for (idx, mut b) in bricks.iter().copied().enumerate() {
-        loop {
-            let mut test = b;
-            test.0.z -= 1;
-            test.1.z -= 1;
-
-            if test.0.z <= 0 || test.1.z <= 0 || ps(test).iter().any(|p| top.contains_key(p)) {
-                break;
-            }
-
-            b = test;
-        }
-
-        for p in ps(b) {
-            top.insert(p, idx);
-        }
-    }
-
-    let mut supporting: HashMap<usize, HashSet<usize>> = HashMap::new();
-    let mut supported_by: HashMap<usize, HashSet<usize>> = HashMap::new();
-
-    let mut ground = HashSet::new();
-
-    for (p, group) in &top {
-        if p.z == 1 {
-            ground.insert(group);
-        }
-
-        let down = Vector3::new(p.x, p.y, p.z - 1);
-        if down.z <= 0 {
-            // eprintln!("{group} => {:?}", supported_by.get(group));
-            supported_by.entry(*group).or_default().insert(usize::MAX);
-        } else if let Some(dg) = top.get(&down) {
-            if dg != group {
-                supporting.entry(*dg).or_default().insert(*group);
-                supported_by.entry(*group).or_default().insert(*dg);
-            }
-        }
-    }
-
-    // dbg!(&supporting, &supported_by);
-    // dbg!(input.lines().count(), bricks.len());
-
-    let mut sum = 0;
-
-    for group in 0..input.lines().count() {
-        let supporting = supporting.get(&group);
-
-        if let Some(supporting) = supporting {
-            if supporting.iter().any(|g| ground.contains(g)) {
-                dbg!(group);
-                dbg!(ground.contains(&group));
-                dbg!(input.lines().nth(group));
-                dbg!(input.lines().nth(72));
-                dbg!(
-                    supporting,
+    (0..bricks.len())
+        .filter(|g| {
+            supporting
+                .get(g)
+                .map(|supporting| {
                     supporting
                         .iter()
-                        .map(|g| supported_by.get(g).unwrap())
-                        .collect_vec(),
-                    supporting.iter().map(|g| ground.contains(g)).collect_vec()
-                );
-                panic!("What??");
-            }
-
-            if supporting
-                .iter()
-                .any(|g| supported_by.get(g).unwrap().contains(&usize::MAX))
-            {
-                panic!("What??>");
-            }
-
-            if supporting
-                .iter()
-                .all(|g| supported_by.get(g).unwrap().len() >= 2)
-            {
-                // eprintln!("{group}");
-                sum += 1;
-            }
-        } else {
-            // eprintln!("{group}");
-            sum += 1;
-        }
-    }
-
-    // 545, 614
-
-    sum
+                        .all(|g| supported_by.get(g).unwrap().len() > 1)
+                })
+                .unwrap_or(true)
+        })
+        .count() as i64
 }
 
 fn part2(input: &str) -> i64 {
-    let mut bricks = input
-        .lines()
-        .map(|l| {
-            let (x1, y1, z1, x2, y2, z2) = l.nums().tup();
-            (Vector3::new(x1, y1, z1), Vector3::new(x2, y2, z2))
-        })
-        .collect_vec();
+    let mut bricks = parse(input);
+    bricks.sort_by_key(|b| b.0.z);
 
-    bricks.sort_by_key(|x| x.0.z);
-
-    let mut top: HashMap<Vector3, usize> = HashMap::new();
-
-    for (idx, mut b) in bricks.iter().copied().enumerate() {
-        loop {
-            let mut test = b;
-            test.0.z -= 1;
-            test.1.z -= 1;
-
-            if test.0.z <= 0 || test.1.z <= 0 || ps(test).iter().any(|p| top.contains_key(p)) {
-                break;
-            }
-
-            b = test;
-        }
-
-        for p in ps(b) {
-            top.insert(p, idx);
-        }
-    }
-
-    let mut supporting: HashMap<usize, HashSet<usize>> = HashMap::new();
-    let mut supported_by: HashMap<usize, HashSet<usize>> = HashMap::new();
-
-    let mut ground = HashSet::new();
-
-    for (p, group) in &top {
-        if p.z == 1 {
-            ground.insert(group);
-        }
-
-        let down = Vector3::new(p.x, p.y, p.z - 1);
-        if down.z <= 0 {
-            // eprintln!("{group} => {:?}", supported_by.get(group));
-            supported_by.entry(*group).or_default().insert(usize::MAX);
-        } else if let Some(dg) = top.get(&down) {
-            if dg != group {
-                supporting.entry(*dg).or_default().insert(*group);
-                supported_by.entry(*group).or_default().insert(*dg);
-            }
-        }
-    }
-
-    // dbg!(&supporting, &supported_by);
-    // dbg!(input.lines().count(), bricks.len());
+    let map = create_map(&bricks);
+    let (supporting, supported_by) = get_supports(&map);
 
     let mut sum = 0;
 
-    for group in 0..input.lines().count() {
+    for group in 0..bricks.len() {
         let mut fallen = HashSet::new();
         fallen.insert(group);
 
@@ -188,23 +113,18 @@ fn part2(input: &str) -> i64 {
                 .iter()
                 .copied()
                 .flat_map(|g| supporting.get(&g).cloned().unwrap_or_default())
+                .filter(|g| !fallen.contains(g))
                 .filter(|g| supported_by.get(g).unwrap().difference(&fallen).count() == 0)
                 .collect();
-                // .collect();
 
-            // dbg!(group, &next);
-
-            if next.difference(&fallen).count() == 0 {
+            if next.is_empty() {
                 sum += fallen.len() - 1;
                 break;
             }
 
             fallen.extend(next);
         }
-
     }
-
-    // 545, 614
 
     sum as i64
 }
