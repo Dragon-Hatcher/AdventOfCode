2acd412bc3a888a4bcad7dae7c4ee392fce6ec0b
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-11-16 11:10:21 -0800

    2015-10

---
+++ solutions/2015/10.rs
@@ -0,0 +1,70 @@
+use advent::prelude::*;
+
+fn default_input() -> &'static str {
+    include_input!(2015 / 10)
+}
+
+fn iterate(digits: &[u8]) -> Vec<u8> {
+    let mut cur_val = digits[0];
+    let mut run_len = 1;
+    let mut out = Vec::new();
+    for &d in digits.iter().skip(1) {
+        if cur_val == d {
+            run_len += 1;
+        } else {
+            out.push(run_len);
+            out.push(cur_val);
+            cur_val = d;
+            run_len = 1;
+        }
+    }
+    out.push(run_len);
+    out.push(cur_val);
+
+    out
+}
+
+fn solve(input: &str, iterations: usize) -> i64 {
+    let mut digits = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
+        .collect_vec();
+
+    for _ in 0..iterations {
+        digits = iterate(&digits);
+    }
+
+    digits.len() as i64
+}
+
+fn part1(input: &str) -> i64 {
+    solve(input, 40)
+}
+
+fn part2(input: &str) -> i64 {
+    solve(input, 50)
+}
+
+fn main() {
+    advent::new(2015, 10, default_input)
+        .part1(part1)
+        .part2(part2)
+        .cli();
+}
+
+#[test]
+fn example() {
+    assert_eq!(iterate(&[1]), vec![1, 1]);
+    assert_eq!(iterate(&[1, 1]), vec![2, 1]);
+    assert_eq!(iterate(&[2, 1]), vec![1, 2, 1, 1]);
+    assert_eq!(iterate(&[1, 2, 1, 1]), vec![1, 1, 1, 2, 2, 1]);
+    assert_eq!(iterate(&[1, 1, 1, 2, 2, 1]), vec![3, 1, 2, 2, 1, 1]);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input), 252594);
+    assert_eq!(part2(input), 3579328);
+}
