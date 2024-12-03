9da41bd9e5ef0ef479492874a4b2c1fb6ffbaa10
Daniel James <danieldragonhatcher@gmail.com>
2024-12-03 15:40:43 -0800

    2018-04 first draft

---
+++ solutions/2018/04.rs
@@ -0,0 +1,117 @@
+use advent::prelude::*;
+
+fn default_input() -> &'static str {
+    include_input!(2018 / 04)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct Date(i64, i64);
+
+#[derive(Debug, Clone, Default)]
+struct Guard(HashMap<Date, HashSet<i64>>);
+
+impl Guard {
+    fn hours_asleep(&self) -> i64 {
+        self.0.values().map(|a| a.len() as i64).sum()
+    }
+
+    fn sleepiest_min(&self) -> (i64, usize) {
+        let (hour, count) = self
+            .0
+            .values()
+            .flat_map(|v| v.iter())
+            .counts()
+            .into_iter()
+            .max_by_key(|(_, c)| *c)
+            .unwrap();
+
+        (*hour, count)            
+    }
+}
+
+fn find_guard_patterns(input: &str) -> HashMap<i64, Guard> {
+    let mut guards: HashMap<i64, Guard> = HashMap::default();
+    let mut active_guard = 0;
+    let mut fell_asleep = None;
+
+    for action in input.lines().sorted() {
+        if action.contains("wakes up") {
+            let (_, month, day, _, min) = action.nums().tup();
+            if let Some(fell_asleep) = fell_asleep {
+                let date = Date(month, day);
+                let asleep = guards
+                    .entry(active_guard)
+                    .or_default()
+                    .0
+                    .entry(date)
+                    .or_default();
+                asleep.extend(fell_asleep..min);
+            }
+        } else if action.contains("falls asleep") {
+            let (_, _, _, _, min) = action.nums().tup();
+            fell_asleep = Some(min);
+        } else {
+            let (_, _, _, _, _, id) = action.nums().tup();
+            active_guard = id;
+            fell_asleep = None;
+        }
+    }
+
+    guards
+}
+
+fn part1(input: &str) -> i64 {
+    let sleepiest_guard = find_guard_patterns(input)
+        .into_iter()
+        .max_by_key(|(_, g)| g.hours_asleep())
+        .unwrap();
+
+    sleepiest_guard.0 * sleepiest_guard.1.sleepiest_min().0
+}
+
+fn part2(input: &str) -> i64 {
+    let sleepiest_time = find_guard_patterns(input)
+        .into_iter()
+        .map(|(id, guard)| (id, guard.sleepiest_min()))
+        .max_by_key(|(_, hour)| hour.1)
+        .unwrap();
+
+    sleepiest_time.0 * sleepiest_time.1.0
+}
+
+fn main() {
+    advent::new(2018, 4, default_input)
+        .part1(part1)
+        .part2(part2)
+        .cli();
+}
+
+#[test]
+fn example() {
+    let input = "[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up";
+    assert_eq!(part1(input), 240);
+    assert_eq!(part2(input), 4455);
+}
+
+#[test]
+fn default() {
+    let input = default_input();
+    assert_eq!(part1(input), 103720);
+    assert_eq!(part2(input), 110913);
+}
