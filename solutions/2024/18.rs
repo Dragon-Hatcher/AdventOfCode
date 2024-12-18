5a060f1f0e3bd74d74e7767eafb142a85a00262a
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-12-17 21:22:56 -0800

    2024-18 clean

--- solutions/2024/18.rs
+++ solutions/2024/18.rs
@@ -1,54 +1,57 @@
 use advent::prelude::*;
 
 fn default_input() -> &'static str {
-    include_input!("width=71\n\n" / 2024 / 18)
+    include_input!("width=71, take=1024\n\n" / 2024 / 18)
+}
+
+fn parse(input: &str) -> (Vec<Vec2>, i64, i64) {
+    let (config, bytes) = input.sections().tup();
+    let (width, take) = config.nums().tup();
+    let bytes = bytes.lines().map(|l| {
+        let (x, y) = l.nums().tup();
+        v2(x, y)
+    }).collect_vec();
+
+    (bytes, width, take)
+}
+
+fn find_path(grid: &Grid<bool>) -> Option<BFSResult<Vec2>> {
+    bfs()
+        .start(Vec2::ZERO)
+        .goal(Vec2::new(grid.width() - 1, grid.height() - 1))
+        .next(|&p| grid.neighbors4(p).filter(|&p| !grid[p]))
+        .try_shortest()
+        .ok()
 }
 
 fn part1(input: &str) -> i64 {
-    let (width, bytes) = input.sections().tup();
-    let width = width.nums().nu();
-    let bytes = bytes.lines().map(|l| {
-        let (x, y) = l.nums().tup();
-        v2(x, y)
-    }).collect_vec();
+    let (bytes, width, take) = parse(input);
 
     let mut grid = Grid::new_homogenous(width, width, false);
-
-    for p in bytes.into_iter().take(1024) {
+    for p in bytes.into_iter().take(take as usize) {
         grid[p] = true;
     }
 
-    println!("{}", grid.pretty());
-    dbg!(v2(width, width));
-
-    bfs()
-        .start(v2(0, 0))
-        .goal(v2(width - 1, width - 1))
-        .next(|p| grid.neighbors4(*p).filter(|p| !grid[*p]))
-        .shortest()
-        .steps
+    find_path(&grid).unwrap().steps
 }
 
-fn part2(input: &str) -> i64 {
-    let (width, bytes) = input.sections().tup();
-    let width = width.nums().nu();
-    let bytes = bytes.lines().map(|l| {
-        let (x, y) = l.nums().tup();
-        v2(x, y)
-    }).collect_vec();
+fn part2(input: &str) -> Vec2 {
+    let (bytes, width, _) = parse(input);
 
     let mut grid = Grid::new_homogenous(width, width, false);
-
-    for p in bytes.into_iter() {
+    let mut last_path = find_path(&grid).unwrap();
+    for p in bytes {
         grid[p] = true;
 
-        if bfs()
-        .start(v2(0, 0))
-        .goal(v2(width - 1, width - 1))
-        .next(|p| grid.neighbors4(*p).filter(|p| !grid[*p]))
-        .try_shortest().is_err() {
-            dbg!(p);
-            return 1;
+        // This didn't disturb our last path so no need to check again.
+        if !last_path.visited.contains_key(&p) {
+            continue;
+        }
+
+        if let Some(path) = find_path(&grid) {
+            last_path = path;
+        } else {
+            return p;
         }
     }
 
@@ -64,7 +67,7 @@ fn main() {
 
 #[test]
 fn example() {
-    let input = "width=7
+    let input = "width=7, take=12
 
 5,4
 4,2
@@ -91,13 +94,13 @@ fn example() {
 0,5
 1,6
 2,0";
-    // assert_eq!(part1(input), 0);
-    assert_eq!(part2(input), 0);
+    assert_eq!(part1(input), 22);
+    assert_eq!(part2(input), Vec2::new(6, 1));
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    // assert_eq!(part1(input), 0);
-    // assert_eq!(part2(input), 0);
+    assert_eq!(part1(input), 282);
+    assert_eq!(part2(input), Vec2::new(64, 29));
 }
