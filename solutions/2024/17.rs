e53f3eb064406bda982686540292d17f952f7592
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-12-17 21:08:41 -0800

    2024-17 clean

--- solutions/2024/17.rs
+++ solutions/2024/17.rs
@@ -4,178 +4,89 @@ fn default_input() -> &'static str {
     include_input!(2024 / 17)
 }
 
-fn run(instructions: &[i64], mut a: i64, mut b: i64, mut c: i64) -> Vec<i64> {
+fn part1(input: &str) -> String {
+    let (start, program) = input.sections().tup();
+    let (mut a, mut b, mut c) = start.nums().tup();
+    let instructions = program.nums().collect_vec();
+
     let mut ip = 0;
     let mut out = vec![];
 
     while ip < instructions.len() {
         let instr = instructions[ip];
-        let operand = instructions[ip + 1];
 
-        // dbg!(ip, instr, operand);
-
-        let combo = match operand {
-            0..=3 => operand,
+        let literal = instructions[ip + 1];
+        let combo = match literal {
+            0..=3 => literal,
             4 => a,
             5 => b,
             _ => c,
         };
 
         match instr {
-            0 => {
-                if combo > 63 {
-                    a = 0;
-                } else {
-                    a = a / 2i64.pow(combo as u32);
-                }
-                ip += 2;
-            }
-            1 => {
-                b = b ^ operand;
-                ip += 2;
-            }
-            2 => {
-                b = combo % 8;
-                ip += 2;
-            }
+            0 => a = a / 2i64.pow(combo.min(64) as u32),
+            1 => b = b ^ literal,
+            2 => b = combo % 8,
             3 => {
                 if a != 0 {
-                    ip = operand as usize;
-                } else {
-                    ip += 2;
+                    ip = literal as usize;
+                    continue;
                 }
             }
-            4 => {
-                b = b ^ c;
-                ip += 2;
-            }
-            5 => {
-                out.push(combo % 8);
-                ip += 2;
-            }
-            6 => {
-                if combo > 63 {
-                    b = 0;
-                } else {
-                    b = a / 2i64.pow(combo as u32);
-                }
-                ip += 2;
-            }
-            _ => {
-                if combo > 63 {
-                    c = 0;
-                } else {
-                    c = a / 2i64.pow(combo as u32);
-                }
-                ip += 2;
-            }
+            4 => b = b ^ c,
+            5 => out.push(combo % 8),
+            6 => b = a / 2i64.pow(combo.min(64) as u32),
+            _ => c = a / 2i64.pow(combo.min(64) as u32),
         }
+
+        ip += 2;
     }
 
-    out
-}
-
-fn part1(input: &str) -> String {
-    let (start, program) = input.sections().tup();
-    let (a, b, c) = start.nums().tup();
-    // let a = 216140366357152;
-    // let a = 216148338629303;
-    let a = 216148338630253;
-    let instructions = program.nums().collect_vec();
-
-    run(&instructions, a, b, c).into_iter().join(",")
-}
-
-fn find(a: i64, targets: &[i64], on: usize, unrev: &[i64]) -> Option<i64> {
-    if on == targets.len() {
-        let a = a / 8;
-        dbg!(a);
-        let mut out = run(&unrev, a, 0, 0);
-        println!("{out:?}");
-        println!("{targets:?}");
-        if out == unrev {
-            return Some(a);
-        } else {
-            return None;
-        }
-    }
-
-    for p in 1..8 {
-        let a = a + p;
-
-        let b1 = (a % 8) ^ 3;
-        let c = a / (2i64.pow(b1 as u32));
-        let b2 = b1 ^ 5 ^ c;
-
-        if b2 % 8 == targets[on] {
-            println!("{on} -> {p}");
-            let found = find(a * 8, targets, on + 1, unrev);
-            if let Some(ans) = found {
-                return Some(ans);
-            }
-        }
-    }
-
-    None
+    out.into_iter().join(",")
 }
 
 fn part2(input: &str) -> i64 {
-    let (start, program) = input.sections().tup();
-    let (_, _, _) = start.nums().tup();
-    let mut instructions = program.nums().collect_vec();
-    let mut targets = instructions.clone();
+    let (_, program) = input.sections().tup();
+    let instructions = program.nums().collect_vec();
 
-    targets.reverse();
-    dbg!(find(0, &targets, 0, &instructions));
+    /*
+    Program: 2,4,1,3,7,5,0,3,1,5,4,1,5,5,3,0
 
-    10
+    while a != 0 {
+        b = a % 8    (2,4)
+        b = b ^ 3    (1,3)
+        c = a / 2**b (7,5)
+        a = a / 8    (0,3)
+        b = b ^ 5    (1,5)
+        b = b ^ c    (4,1)
+        print b % 8  (5,5)
+    }
+    */
 
-    // let mut a = 0;
+    fn find_quine(a: i64, instructions: &[i64], on: usize) -> Option<i64> {
+        for p in 1..8 {
+            let a = a + p;
 
-    // for target in instructions.into_iter().rev() {
-    //     a = a * 8;
-    //     let start = a;
+            let b1 = (a % 8) ^ 3;
+            let c = a / (2i64.pow(b1 as u32));
+            let b2 = b1 ^ 5 ^ c;
 
-    //     loop {
-    //         a += 1;
+            if b2 % 8 == instructions[on] {
+                if on == 0 {
+                    return Some(a);
+                }
 
-    //         let b1 = (a % 8) ^ 3;
-    //         let c = a / (2i64.pow(b1 as u32));
-    //         let b2 = b1 ^ 5 ^ c;
+                if let ans @ Some(_) = find_quine(a * 8, instructions, on - 1) {
+                    return ans;
+                }
+            }
+        }
 
-    //         if b2 % 8 == target {
-    //             break;
-    //         }
+        None
+    }
 
-    //     }
-
-    //     dbg!(a - start);
-
-    // }
-
-    // a
+    find_quine(0, &instructions, instructions.len() - 1).unwrap()
 }
-
-/*
-
-Register A: 63687530
-Register B: 0
-Register C: 0
-
-Program: 2,4,1,3,7,5,0,3,1,5,4,1,5,5,3,0
-
-while a != 0 {
-    b = a % 8    (2,4)
-    b = b ^ 3    (1,3)
-    c = a / 2**b  (7,5)
-    a = a / 8    (0,3)
-    b = b ^ 5    (1,5)
-    b = b ^ c    (4,1)
-    print b % 8  (5,5)
-}
-
-
-*/
 
 fn main() {
     advent::new(2024, 17, default_input)
@@ -191,13 +102,12 @@ Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0";
-    // assert_eq!(part1(input), 0);
-    // assert_eq!(part2(input), 0);
+    assert_eq!(part1(input), "4,6,3,5,6,3,5,2,1,0");
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    // assert_eq!(part1(input), 0);
-    // assert_eq!(part2(input), 0);
+    assert_eq!(part1(input), "1,6,7,4,3,0,5,0,6");
+    assert_eq!(part2(input), 216148338630253);
 }
