f2ad3fa1f9bb3da836b9b23dafb8d39e30b0625d
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2024-12-21 12:16:08 -0800

    2024-21 clean

--- solutions/2024/21.rs
+++ solutions/2024/21.rs
@@ -1,20 +1,8 @@
-use std::{
-    i64,
-    iter::{empty, once},
-    sync::OnceLock,
-};
-
 use advent::prelude::*;
+use std::{i64, iter::once, sync::OnceLock};
 
 fn default_input() -> &'static str {
     include_input!(2024 / 21)
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
-struct State {
-    dirs: Vec<Vec2>,
-    num: Vec2,
-    left: String,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -65,137 +53,93 @@ fn num_pad() -> &'static HashMap<char, Vec2> {
     })
 }
 
+fn find_cost_from_to(
+    from: Vec2,
+    to: Vec2,
+    costs: &HashMap<(Action, Action), i64>,
+    valid_squares: &HashSet<Vec2>,
+) -> i64 {
+    let res = dijkstra()
+        .start((from, Action::Press))
+        .goal((to, Action::Press))
+        .next(|&(at, last_act)| {
+            Action::ALL.into_iter().filter_map(move |&act| {
+                let cost = costs[&(last_act, act)];
+                match act {
+                    Action::Press => Some(((at, act), cost)),
+                    Action::Move(d) => {
+                        let next = at + d.vector();
+                        valid_squares.contains(&next).then_some(((next, act), cost))
+                    }
+                }
+            })
+        })
+        .search();
+
+    res.dists[&res.stop_node.unwrap()]
+}
+
 fn solve_one(goal: &str, n: usize) -> i64 {
     let mut transition_costs: HashMap<(Action, Action), i64> = HashMap::default();
 
+    // Initially the cost of any action is one button press on the top most
+    // direction pad.
     for &p1 in dir_pad().keys() {
         for &p2 in dir_pad().keys() {
             transition_costs.insert((p1, p2), 1);
         }
     }
 
+    let valid_dir_squares = dir_pad().values().copied().collect();
+    let valid_num_squares = num_pad().values().copied().collect();
+
+    // But now we see how much each transition costs for each additional
+    // intermediate direction pad.
     for _ in 0..n {
         let mut new_trans_costs = HashMap::default();
 
         for &(from, to) in transition_costs.keys() {
-            let from_p = dir_pad()[&from];
-            let to_p = dir_pad()[&to];
+            if from == to {
+                // We just press the press button again.
+                new_trans_costs.insert((from, to), 1);
+                continue;
+            }
 
-            let res = dijkstra()
-                .start((from_p, Action::Press))
-                .goal((to_p, Action::Press))
-                .next(|&(at, last_act)| {
-                    let mut res = vec![];
-
-                    for &act in Action::ALL {
-                        let cost = transition_costs[&(last_act, act)];
-                        match act {
-                            Action::Press => {
-                                if at == to_p {
-                                    res.push(((at, act), cost));
-                                }
-                            }
-                            Action::Move(d) => {
-                                let next = at + d.vector();
-                                if dir_pad().values().contains(&next) {
-                                    res.push(((next, act), cost));
-                                }
-                            }
-                        }
-                    }
-
-                    res.into_iter()
-                })
-                .search();
-            let dist = res.dists[&res.stop_node.unwrap()].max(1);
-            // println!("{from:?} ---({dist})--> {to:?}");
-
-            new_trans_costs.insert((from, to), dist);
+            new_trans_costs.insert(
+                (from, to),
+                find_cost_from_to(
+                    dir_pad()[&from],
+                    dir_pad()[&to],
+                    &transition_costs,
+                    &valid_dir_squares,
+                ),
+            );
         }
 
         transition_costs = new_trans_costs;
     }
 
-    let mut cost = 0;
-    for (from, to) in once('A').chain(goal.chars()).tuple_windows() {
-        let from = num_pad()[&from];
-        let to = num_pad()[&to];
-
-        let res = dijkstra()
-            .start((from, Action::Press))
-            .goal((to, Action::Press))
-            .next(|&(at, last_act)| {
-                let mut res = vec![];
-
-                for &act in Action::ALL {
-                    let cost = transition_costs[&(last_act, act)];
-                    match act {
-                        Action::Press => {
-                            if at == to {
-                                res.push(((at, act), cost));
-                            }
-                        }
-                        Action::Move(d) => {
-                            let next = at + d.vector();
-                            if num_pad().values().contains(&next) {
-                                res.push(((next, act), cost));
-                            }
-                        }
-                    }
-                }
-
-                res.into_iter()
-            })
-            .search();
-        let dist = res.dists[&res.stop_node.unwrap()];
-        cost += dist;
-        // println!("{from} ---({dist})--> {to}");
-    }
-
-    cost
-
-    // let mut open = MinBinaryHeap::new_min();
-    // let mut seen = HashSet::default();
-    // let mut g_score = HashMap::default();
-
-    // let start = State::start(goal.to_owned(), n);
-    // open.push((0, start.clone()));
-    // g_score.insert(start.clone(), 0);
-    // seen.insert(start.clone());
-
-    // let mut lowest_seen = start.left.len();
-
-    // while let Some((_, curr)) = open.pop() {
-    //     let my_dist = g_score[&curr];
-
-    //     if curr.left.is_empty() {
-    //         return my_dist;
-    //     }
-
-    //     for n in Action::ALL
-    //         .into_iter()
-    //         .filter_map(move |act| curr.clone().tick_dir(*act, 0))
-    //     {
-    //         if !seen.contains(&n) {
-    //             seen.insert(n.clone());
-    //             g_score.insert(n.clone(), my_dist + 1);
-    //             let h = (n.left.len() as i64);
-    //             open.push((my_dist + 1 + h, n));
-    //         }
-    //     }
-    // }
-
-    // unreachable!()
+    // And finally we get the cost to do the required sequence of moves on the
+    // number pad.
+    once('A')
+        .chain(goal.chars())
+        .tuple_windows()
+        .map(|(from, to)| {
+            find_cost_from_to(
+                num_pad()[&from],
+                num_pad()[&to],
+                &transition_costs,
+                &valid_num_squares,
+            )
+        })
+        .sum()
 }
 
 fn solve(input: &str, n: usize) -> i64 {
-    let mut sum = 0;
-    for l in input.lines() {
-        let num = l.nums().nu();
-        let len = solve_one(l.trim(), n);
-        sum += len * num;
-    }
-    sum
+    input
+        .lines()
+        .map(|code| code.nums().nu() * solve_one(code, n))
+        .sum()
 }
 
 fn part1(input: &str) -> i64 {
@@ -215,14 +159,17 @@ fn main() {
 
 #[test]
 fn example() {
-    let input = "029A";
-    assert_eq!(part1(input), 0);
-    // assert_eq!(part2(input), 0);
+    let input = "029A
+980A
+179A
+456A
+379A";
+    assert_eq!(part1(input), 126384);
 }
 
 #[test]
 fn default() {
     let input = default_input();
-    // assert_eq!(part1(input), 0);
-    // assert_eq!(part2(input), 0);
+    assert_eq!(part1(input), 231564);
+    assert_eq!(part2(input), 281212077733592);
 }
